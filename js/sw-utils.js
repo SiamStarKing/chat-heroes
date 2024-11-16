de2623594a5bcde7b4c65d22f28f865ba5bf5f22
Siam <siamgama19@gmail.com>
2024-11-15 21:41:32 -0600

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+function actualizarCacheDinamico(dynamicCache, req, res){
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache =>{
+            cache.put(req, res.clone());
+            return res.clone();
+        }); // Cierre then
+    } // Cierre if
+    else{
+        return res;
+    }
+} // Cierre función
